a37067fa39f7a8c7dad37646b0d753aab1bc1f4c
GUGLIELMO BARTELLONI <guglielmo.bartelloni@intesasanpaolo.com>
2024-07-13 16:27:30 +0200

    feat: first application layout

---
+++ src/main/java/com/yanchware/assesment/user/UserService.java
@@ -0,0 +1,26 @@
+package com.yanchware.assesment.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserService {
+
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void saveUser(User user) {
+        String encodedKey = passwordEncoder.encode(user.getPrivateKey());
+        String encodedAwsAccessKey = passwordEncoder.encode(user.getAwsAccessKey());
+        String encodedAwsSecretKey = passwordEncoder.encode(user.getAwsSecretKey());
+        user.setPrivateKey(encodedKey);
+        user.setAwsAccessKey(encodedAwsAccessKey);
+        user.setAwsSecretKey(encodedAwsSecretKey);
+        userRepository.save(user);
+    }
+}
